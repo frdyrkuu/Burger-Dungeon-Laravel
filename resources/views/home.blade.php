04f45e91d53dfa6594bfb5633fd18624da6b13c7
Frdyrkuu <socorinzyk@gmail.com>
2023-03-21 13:18:00 +0800

    upload - updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     @vite('resources/css/app.css')
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -17,16 +16,37 @@
                         @endif
                         {{ __('You are logged in!') }}
 
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+
+                    <div class="card mt-2 rounded-2xl h-full">
+                        <h1 class="text-sm mb-2 card-header text-gray-900">Upload</h1>
+                        <div class="p-6">
+                            <form method="POST" action="/upload" enctype="multipart/form-data">
+                                @csrf
+                                <input type="file" name="image" accept="image/png" onchange="loadFile(event)" required>
+                                <div class="w-64 h-64 flex justify-center items-center m-auto mt-2">
+                                    <img src="/img/upload-icon.png" alt=""
+                                        class="h-64 my-4 m-auto  object-center w-64" id="output">
+                                </div>
+                                <input type="submit" name="upload"
+                                    class="bg-orange-500 rounded-2xl px-4 py-2 text-white w-full sm:mb-0 mb-10 mt-2">
+                            </form>
+                        </div>
+                    </div>
+
+
+                    <div class="card mt-2 rounded-2xl h-11/12">
+                        <h1 class="text-sm mb-2 card-header text-gray-900">Details</h1>
 
                     </div>
-                    <h1>Upload</h1>
-                    <form method="POST" action="/upload" enctype="multipart/form-data">
-                        @csrf
-                        <input type="file" name="image">
-                        <input type="submit" name="upload">
-                    </form>
+
+
                 </div>
             </div>
         </div>
     </div>
 @endsection
+<script src="/display.js"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-
+    <link rel="stylesheet" href="/header.css">
     <!-- Scripts -->
     @viteReactRefresh
     @viteReactRefresh
@@ -25,58 +25,6 @@
 </head>
 
 <body class="font-['Poppins']">
-    {{-- <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-orange-900 shadow-sm">
-            <div class="container">
-                <a class="navbar-brand font-bold text-white text-4xl" href="{{ url('/') }}">
-                    <span class="text-orange-500">bu</span>rger dungeon.
-                </a>
-
-                <div class="collapse navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-
-
-
-
-                </div>
-
-            </div>
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto px-10">
-                <!-- Authentication Links -->
-                @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link text-white text-2sm" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link text-white text-2sm" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white text-2xl mr-4" href="#"
-                            role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-front w-11/12" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-
-        </nav> --}}
 
     <header class="bg-orange-900 text-white sticky top-0 z-50 font-['Poppins'] dark:bg-white dark:text-orange-900">
         <section class="max-w-7xl mx-auto p-4 flex justify-between items-center">
@@ -89,44 +37,43 @@
                 </button>
                 <nav class="hidden sm:block space-x-8 text-xl font-semibold" aria-label="main">
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto px-10 flex flex-row">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link text-white text-2sm"
-                                            href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link text-white text-2sm ml-5"
-                                            href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-white text-2xl mr-4"
-                                        href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-front w-11/12" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto px-10 flex flex-row">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-white text-2sm"
+                                        href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endguest
-                        </ul>
+                            @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-white text-2sm ml-5"
+                                        href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white text-2xl mr-4"
+                                    href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-front w-11/12" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
 
                 </nav>
             </div>
